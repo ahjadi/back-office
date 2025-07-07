ee93d7f692e15b3356ceabfde159204a627d56a6
ahjadi <alihussainaljadi@gmail.com>
2025-07-07 21:01:54 +0300

    validation(dob): implement custom date of birth validator

---
+++ src/main/kotlin/com/nbk/task/backoffice/service/validation/DateTimeValid.kt
@@ -0,0 +1,21 @@
+package com.nbk.task.backoffice.service.validation
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [DateTimeValidator::class])
+
+annotation class DateTimeValid(
+    val message: String = "Invalid date format",
+    val format: String = "dd-MM-yyyy",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+/*
+* to use at the entity level:
+* @field:DateTimeValid(format = "dd-MM-yyyy", message = "dd-MM-yyyy")
+* */
